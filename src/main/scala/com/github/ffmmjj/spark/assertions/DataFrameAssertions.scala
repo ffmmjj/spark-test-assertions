4625651c5288bb1906901971707678f92717bb13
Felipe Martins <ffmmjj@gmail.com>
2019-02-08 21:49:37 -0300

    Assert on columns with different ordering

--- src/main/scala/com/github/ffmmjj/spark/assertions/DataFrameAssertions.scala
+++ src/main/scala/com/github/ffmmjj/spark/assertions/DataFrameAssertions.scala
@@ -9,12 +9,26 @@ object DataFrameAssertions {
 
 case class DataFrameWithCustomAssertions(actual: DataFrame) {
   def shouldHaveSameContentsAs(expected: DataFrame): Unit = {
-    val expectedDfColumns = expected.columns.toSet
-    val actualDfColumns = actual.columns.toSet
-    val missingColumnsInActualDf = expectedDfColumns.diff(actualDfColumns).toSeq
-    val extraColumnInActualDf = actualDfColumns.diff(expectedDfColumns).toSeq
+    val expectedDfColumns = expected.columns
+    val actualDfColumns = actual.columns
+    val missingColumnsInActualDf = expectedDfColumns.toSet.diff(actualDfColumns.toSet).toSeq
+    val extraColumnInActualDf = actualDfColumns.toSet.diff(expectedDfColumns.toSet).toSeq
 
-    assert(missingColumnsInActualDf.isEmpty, s"Dataframe ${actual.toString} doesn't have column(s) [${String.join(", ", missingColumnsInActualDf:_*)}]")
-    assert(extraColumnInActualDf.isEmpty, s"Dataframe ${actual.toString} contains extra columns [${String.join(", ", extraColumnInActualDf:_*)}]")
+    assert(missingColumnsInActualDf.isEmpty, buildMissingColumnsMessage(missingColumnsInActualDf))
+    assert(extraColumnInActualDf.isEmpty, buildExtraColumnsMessage(extraColumnInActualDf))
+    assert(actualDfColumns sameElements expectedDfColumns, buildColumnsInDifferentOrderMessage(expected))
+  }
+
+  private def buildMissingColumnsMessage(missingColumnsInActualDf: Seq[String]) = {
+    s"Dataframe ${actual.toString} doesn't have column(s) [${String.join(", ", missingColumnsInActualDf: _*)}]"
+  }
+
+  private def buildExtraColumnsMessage(extraColumnInActualDf: Seq[String]) = {
+    s"Dataframe ${actual.toString} contains extra columns [${String.join(", ", extraColumnInActualDf: _*)}]"
+  }
+
+  private def buildColumnsInDifferentOrderMessage(expected: DataFrame) = {
+    s"DataFrame ${actual.toString} has the same columns as ${expected.toString}, but in a different order - " +
+      s"do you really care about column order in this test?"
   }
 }
--- src/test/scala/com/github/ffmmjj/spark/assertions/DataFrameAssertionsTest.scala
+++ src/test/scala/com/github/ffmmjj/spark/assertions/DataFrameAssertionsTest.scala
@@ -13,7 +13,7 @@ class DataFrameAssertionsTest extends FlatSpec with SparkSessionTestWrapper with
   behavior of "shouldHaveSameContentsAs"
   it should "raise an exception detailing the missing fields if the expected dataframe has columns that don't exist in the actual dataframe" in {
     val actual = Seq("value1").toDF("field1")
-    val expected = Seq(("value4", "value5", "value6")).toDF("field1", "field2", "field3")
+    val expected = Seq(("value1", "value2", "value3")).toDF("field1", "field2", "field3")
 
     val assertionResult = Try(actual shouldHaveSameContentsAs expected)
 
@@ -23,11 +23,21 @@ class DataFrameAssertionsTest extends FlatSpec with SparkSessionTestWrapper with
 
   it should "raise an exception detailing the extra fields if the actual dataframe has columns that dont't exist in the expected dataframe" in {
     val actual = Seq(("value1", "value2", "value3")).toDF("field1", "field2", "field3")
-    val expected = Seq("value4").toDF("field1")
+    val expected = Seq("value1").toDF("field1")
 
     val assertionResult = Try(actual shouldHaveSameContentsAs expected)
 
     val expectedErrorMessage = s"${actual.toString()} contains extra columns [field2, field3]"
+    assertionResult.failed.get.getMessage should include (expectedErrorMessage)
+  }
+
+  it should "raise an exception if the columns in the actual and expected dataframes follow a different order" in {
+    val actual = Seq(("value1", "value2")).toDF("field1", "field2")
+    val expected = Seq(("value2", "value1")).toDF("field2", "field1")
+
+    val assertionResult = Try(actual shouldHaveSameContentsAs expected)
+
+    val expectedErrorMessage = s"${actual.toString} has the same columns as ${expected.toString}, but in a different order - do you really care about column order in this test?"
     assertionResult.failed.get.getMessage should include (expectedErrorMessage)
   }
 }
